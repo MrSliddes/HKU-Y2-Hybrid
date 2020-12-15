107d68cd0b533709f1df62768ccd587486c59af4
MrSliddes <mrsliddes@outlook.com>
2020-12-15 16:56:18 +0100

    Props implemented

---
+++ HKU-Y2-Hybrid/Assets/Scripts/Scripts Tymon/WebCamDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamDisplay : MonoBehaviour
+{
+    private WebCamTexture webCamTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        this.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+        StartCoroutine(CheckCam());
+    }
+
+    IEnumerator CheckCam()
+    {
+        while(true)
+        {
+            if(!webCamTexture.isPlaying) webCamTexture.Play();
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
