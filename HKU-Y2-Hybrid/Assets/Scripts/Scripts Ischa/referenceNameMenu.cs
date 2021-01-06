e451a4966811b8fd44369e5c29f6681fba8043bb
Ischa-w <ischa@westerveld.art>
2021-01-06 15:41:58 +0100

    UI Verder gemaakt. Singleton Script gemaakt met de naam in een string, die niet destroyed wordt naar volgende scene. Hierdoor kan de naam gebruikt worden in het spel. Dit is nog niet de mooiste oplossing.

---
+++ HKU-Y2-Hybrid/Assets/Scripts/Scripts Ischa/referenceNameMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class referenceNameMenu : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<TMPro.TextMeshProUGUI>().text = InfoHolderMenu.theName;
+    }
+}
