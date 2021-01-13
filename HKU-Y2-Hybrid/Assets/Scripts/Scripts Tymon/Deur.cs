5c54b8f53ca156138e44b3caac345019f216ba23
MrSliddes <mrsliddes@outlook.com>
2021-01-13 14:53:34 +0100

    Commit Polish 0

---
+++ HKU-Y2-Hybrid/Assets/Scripts/Scripts Tymon/Deur.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deur : MonoBehaviour, IInteractable
+{ 
+
+    public bool isOpen = false;
+
+    public Animator animator;
+    public string animOpenName;
+    public string animCloseName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDeInteract()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnInteract(Transform parent)
+    {
+        if(isOpen)
+        {
+            // Close
+            animator.Play(animCloseName);
+        }
+        else
+        {
+            // Open
+            animator.Play(animOpenName);
+        }
+        isOpen = !isOpen;
+        parent.GetComponentInParent<Player>().itemPickedUp = null;
+    }
+}
